e2857f72a12be8ca56b9367b435d62057ac3e378
1882COYS <98269518+1882COYS@users.noreply.github.com>
2023-12-29 10:16:28 +0000

    Add files via upload

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ sleep 3
 
 echo INSTALLING PACMAN PREQUISITS...
 
-sudo pacman -S neofetch thunar dunst kvantum rofi swaylock waybar wofi ccache qt6ct qt6-wayland qt5-graphicaleffects qt5-svg qt5-quickcontrols2 zsh kitty pamixer brightnessctl swaybg
+sudo pacman -S neofetch thunar dunst kvantum waybar wofi ccache qt6ct qt6-wayland qt5-graphicaleffects qt5-svg qt5-quickcontrols2 zsh kitty pamixer brightnessctl swaybg
 
 echo INSTALLING AUR PREQUISITS...
 
