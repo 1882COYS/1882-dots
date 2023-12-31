5f1d618f14ae72d4b04209e57593d500dd02e725
1882COYS <98269518+1882COYS@users.noreply.github.com>
2023-12-31 14:02:24 +0000

    Add files via upload

--- install.sh
+++ install.sh
@@ -46,8 +46,9 @@ cp -r waybar ~/.config
 cp -r wlogout ~/.config
 cp -r compton.conf ~/.config
 cp -r kitty ~/.config
+mkdir ~/Pictures
 cp -r home.jpg ~/Pictures
-cp -r .zshrc ~/
+
 cp -r .p10k.zsh ~/
 
 sudo chmod +x ~/.config/waybar/temp.py
@@ -72,6 +73,7 @@ sudo cp -r sddm.conf /etc
 
 echo Refer back to github page to complete.
 
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
