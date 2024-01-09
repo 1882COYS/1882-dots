626fbf4046c272ed00fa895460ca7cf8611ca9f8
1882COYS <98269518+1882COYS@users.noreply.github.com>
2024-01-09 12:08:05 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -73,7 +73,7 @@ sudo cp -r sddm.conf /etc
 
 echo Refer back to github page to complete.
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
